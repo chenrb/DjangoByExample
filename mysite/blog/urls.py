e2a6f159da6cb51d472122e1c5a3e0c0eba68b10
chenrb <410618634@qq.com>
2018-03-23 00:07:14 +0800

    完成第一章，简单的博客系统

---
+++ mysite/blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*- 
+__author__ = 'John 2018/3/22 22:24'
+
+from django.urls import path
+
+from . import views
+
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
+]
