5d899964ac41133f45d18c71abf57af743566872
chenrb <410618634@qq.com>
2018-03-25 12:02:58 +0800

    删除忽略项

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -3,9 +3,17 @@ __author__ = 'John 2018/3/23 15:41'
 
 from django import forms
 
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
