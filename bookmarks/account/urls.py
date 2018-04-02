5f417878527e492eac463bd2e1cdb4941e05d627
chenrb <410618634@qq.com>
2018-04-02 23:30:31 +0800

    使用django自带的认证view

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -2,10 +2,30 @@
 __author__ = 'John 2018/4/1 23:16'
 
 from django.urls import path
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
+from django.contrib.auth.views import password_reset
+from django.contrib.auth.views import password_reset_done
+from django.contrib.auth.views import password_reset_confirm
+from django.contrib.auth.views import password_reset_complete
 
-from .views import user_login
+from . import views
 
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('logout-then-login/', logout_then_login, name='logout_then_login'),
+    path('password-change/', password_change, name='password_change'),
+    path('password-change/done/', password_change_done, name='password_change'),
+    path('password-reset/', password_reset, name='password_reset'),
+    path('password-reset/done/', password_reset_done, name='password_reset_done'),
+    path('password-reset-confirm/<str:uidb64>/<str:token>/', password_reset_confirm, name='password_reset_confirm'),
+    path('password-reset/complete/', password_reset_complete, name='password_reset_complete'),
+
+    path('', views.dashboard, name='dashboard'),
 ]
